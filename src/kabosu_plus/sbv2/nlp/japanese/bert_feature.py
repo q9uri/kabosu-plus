a161a1d4b4bbabe27dd108ab28c7b60b7540daf6
q9uri <9ri.nasu@gmail.com>
2025-09-27 11:43:37 +0900

    back before ort

--- src/kabosu_plus/sbv2/nlp/japanese/bert_feature.py
+++ src/kabosu_plus/sbv2/nlp/japanese/bert_feature.py
@@ -12,13 +12,13 @@ from kabosu_plus.sbv2.nlp import onnx_bert_models
 from kabosu_plus.sbv2.utils import get_onnx_device_options
 from kabosu_plus.sbv2.nlp.japanese.g2p import text_to_sep_kata
 
-import torch
 
 
 
-def extract_bert_feature(
+def extract_bert_feature_onnx(
     text: str,
     word2ph: list[int],
+    onnx_providers: Sequence[Union[str, tuple[str, dict[str, Any]]]],
     assist_text: Optional[str] = None,
     assist_text_weight: float = 0.7,
 ) -> NDArray[Any]:
@@ -44,18 +44,54 @@ def extract_bert_feature(
 
     # トークナイザーとモデルの読み込み
     tokenizer = onnx_bert_models.load_tokenizer(Languages.JP)
-    model = onnx_bert_models.load_model(
+    session = onnx_bert_models.load_model(
         language=Languages.JP,
     )
+    input_names = [input.name for input in session.get_inputs()]
+    output_name = session.get_outputs()[0].name
+
+    # 入力テンソルの転送に使用するデバイス種別, デバイス ID, 実行オプションを取得
+    device_type, device_id, run_options = get_onnx_device_options(session, onnx_providers)  # fmt: skip
+
     # 入力をテンソルに変換
-    inputs = tokenizer(text, return_tensors="pt")
-    res = model(**inputs)
+    inputs = tokenizer(text, return_tensors="np")
+    input_tensor = [
+        inputs["input_ids"].astype(np.int64),  # type: ignore
+        inputs["attention_mask"].astype(np.int64),  # type: ignore
+    ]
+    # 推論デバイスに入力テンソルを割り当て
+    ## GPU 推論の場合、device_type + device_id に対応する GPU デバイスに入力テンソルが割り当てられる
+    io_binding = session.io_binding()
+    for name, value in zip(input_names, input_tensor):
+        gpu_tensor = onnxruntime.OrtValue.ortvalue_from_numpy(
+            value, device_type, device_id
+        )
+        io_binding.bind_ortvalue_input(name, gpu_tensor)
+    # text から BERT 特徴量を抽出
+    io_binding.bind_output(output_name, device_type)
+    session.run_with_iobinding(io_binding, run_options=run_options)
+    res = io_binding.get_outputs().numpy().astype(np.float32)
 
     style_res_mean = None
     if assist_text:
         # 入力をテンソルに変換
-        style_inputs = tokenizer(assist_text, return_tensors="tp")
-        style_res = model(**style_inputs)
+        style_inputs = tokenizer(assist_text, return_tensors="np")
+        style_input_tensor = [
+            style_inputs["input_ids"].astype(np.int64),  # type: ignore
+            style_inputs["attention_mask"].astype(np.int64),  # type: ignore
+        ]
+        # 推論デバイスに入力テンソルを割り当て
+        ## GPU 推論の場合、device_type + device_id に対応する GPU デバイスに入力テンソルが割り当てられる
+        io_binding = session.io_binding()  # IOBinding は作り直す必要がある
+        for name, value in zip(input_names, style_input_tensor):
+            gpu_tensor = onnxruntime.OrtValue.ortvalue_from_numpy(
+                value, device_type, device_id
+            )
+            io_binding.bind_ortvalue_input(name, gpu_tensor)
+        # assist_text から BERT 特徴量を抽出
+        io_binding.bind_output(output_name, device_type)
+        session.run_with_iobinding(io_binding, run_options=run_options)
+        style_res = io_binding.get_outputs()[0].numpy().astype(np.float32)
         style_res_mean = np.mean(style_res, axis=0)
 
     assert len(word2ph) == len(text) + 2, text
