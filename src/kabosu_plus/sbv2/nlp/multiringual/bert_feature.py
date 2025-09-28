d4c10317d35cef61cb2fbc7e1fdc227b72672855
q9uri <9ri.nasu@gmail.com>
2025-09-28 15:32:29 +0900

    change create_embedding to embed

--- src/kabosu_plus/sbv2/nlp/multiringual/bert_feature.py
+++ src/kabosu_plus/sbv2/nlp/multiringual/bert_feature.py
@@ -46,7 +46,7 @@ def extract_bert_feature_lammacpp(
         language=Languages.MULTI,
     )
 
-    res = model.create_embedding(text).numpy()
+    res = model.embed(text).numpy()
 
     style_res_mean = None
     if assist_text:
