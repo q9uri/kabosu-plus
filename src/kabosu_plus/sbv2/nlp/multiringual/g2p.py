dce8fa661f79b44268f611ba05ea688164ff8ef9
q9uri <9ri.nasu@gmail.com>
2025-09-29 13:06:03 +0900

    コードをきれいに

--- src/kabosu_plus/sbv2/nlp/multiringual/g2p.py
+++ src/kabosu_plus/sbv2/nlp/multiringual/g2p.py
@@ -6,7 +6,7 @@ def g2p(text: str,
         babytalk: bool = False, 
         dakuten: bool = False,
         language_list:list[Languages] = [Languages.JP],
-        ) -> tuple[Languages, list[str], list[int], list[int], list[str] | None, list[str] | None, list[str] | None]:
+        ) -> tuple[Languages, str, list[str], list[int], list[int], list[str] | None, list[str] | None, list[str] | None]:
 
     if len(language_list) == 1:
         language = language_list[0] 
@@ -26,12 +26,17 @@ def g2p(text: str,
     elif language == Languages.EN:
         from kabosu_plus.sbv2.nlp.english import g2p as g2p_en 
 
-        norm_text,  phones, tones, word2ph = g2p_en.g2p(text=text)
-        return Languages.EN, norm_text, phones, tones, word2ph, None, None, None
+        norm_text, phones, tones, word2ph = g2p_en.g2p(text=text)
+        sep_text = None
+        sep_kata = None
+        sep_kata_with_joshi = None
     
     elif language == Languages.ZH:
         from kabosu_plus.sbv2.nlp.chinese import g2p as g2p_zh
 
         norm_text, phones, tones, word2ph = g2p_zh.g2p(text=text)
+        sep_text = None
+        sep_kata = None
+        sep_kata_with_joshi = None
 
-        return Languages.ZH, norm_text, phones, tones, word2ph, None, None, None
+    return language, norm_text, phones, tones, word2ph, sep_text, sep_kata ,sep_kata_with_joshi
