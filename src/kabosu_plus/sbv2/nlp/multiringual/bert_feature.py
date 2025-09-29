b28291ad872dbcdd7ff17c1fdd08d98b755d5925
q9uri <9ri.nasu@gmail.com>
2025-09-29 11:55:22 +0900

    extrac bert feature llamacppを日本語以外に対応

--- src/kabosu_plus/sbv2/nlp/multiringual/bert_feature.py
+++ src/kabosu_plus/sbv2/nlp/multiringual/bert_feature.py
@@ -8,7 +8,6 @@ from numpy.typing import NDArray
 
 from kabosu_plus.sbv2.constants import Languages
 from kabosu_plus.sbv2.nlp import llamacpp_embedding_models
-from kabosu_plus.sbv2.utils import get_onnx_device_options
 from kabosu_plus.sbv2.nlp.japanese.g2p import text_to_sep_kata
 
 
@@ -34,18 +33,11 @@ def extract_bert_feature_lammacpp(
         NDArray[Any]: BERT の特徴量
     """
 
-    # 各単語が何文字かを作る `word2ph` を使う必要があるので、読めない文字は必ず無視する
-    # でないと `word2ph` の結果とテキストの文字数結果が整合性が取れない
-    text = "".join(text_to_sep_kata(text, raise_yomi_error=False)[0])
-    if assist_text:
-        assist_text = "".join(text_to_sep_kata(assist_text, raise_yomi_error=False)[0])
-
     # モデルの読み込み
     model = llamacpp_embedding_models.load_model(
         language=Languages.MULTI,
     )
 
-    style_res_mean = None
     if assist_text:
         # 入力をテンソルに変換
         
@@ -59,25 +51,25 @@ def extract_bert_feature_lammacpp(
 
 
     assert len(word2ph) == len(text) + 2, text
-    word2phone = word2ph
+
     phone_level_feature = []
-    for i in range(len(word2phone)):
+    for i in range(len(word2ph)):
         #先頭と終端のみ埋め込みを入れる
-        if i in (0, len(word2phone)-1 ):
+        if i in (0, len(word2ph)-1 ):
 
             if assist_text:
                 assert style_res is not None
                 repeat_feature = (
-                    np.tile(res, (word2phone[i], 1)) * (1 - assist_text_weight)
-                    + np.tile(style_res, (word2phone[i], 1)) * assist_text_weight
+                    np.tile(res, (word2ph[i], 1)) * (1 - assist_text_weight)
+                    + np.tile(style_res, (word2ph[i], 1)) * assist_text_weight
                 )
             else:
                 #先頭と終端のみ埋め込みを入れる
-                repeat_feature = np.tile(res, (word2phone[i], 1))
+                repeat_feature = np.tile(res, (word2ph[i], 1))
 
         else:
             #先頭と終端以外は0を入れる
-            repeat_feature = np.tile(zero_array, (word2phone[i], 1))
+            repeat_feature = np.tile(zero_array, (word2ph[i], 1))
 
         phone_level_feature.append(repeat_feature)
 
